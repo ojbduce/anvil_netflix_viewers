5d6dc6f89b86eab3b0ede71308817567c62ad071
oliverduce@googlemail.com <oliverduce@googlemail.com>
2023-08-18 04:00:58 +0000

    Created Base_Template and custom package dependencies; Edited settings; Deleted Form1

--- client_code/Base_Template/__init__.py
+++ client_code/Base_Template/__init__.py
@@ -1,9 +1,10 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import Base_TemplateTemplate
 from anvil import *
 
-class Form1(Form1Template):
+class Base_Template(Base_TemplateTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    
 
     # Any code you write here will run before the form opens.
