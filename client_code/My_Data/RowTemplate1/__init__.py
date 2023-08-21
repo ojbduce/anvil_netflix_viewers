a14ce146eb8b4aee53442bbfc7c7e6ee5a4762e3
oliverduce@googlemail.com <oliverduce@googlemail.com>
2023-08-21 05:10:16 +0000

    Edited My_Data and My_Data.RowTemplate1

--- client_code/My_Data/RowTemplate1/__init__.py
+++ client_code/My_Data/RowTemplate1/__init__.py
@@ -4,6 +4,7 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+import anvil.media
 
 class RowTemplate1(RowTemplate1Template):
   def __init__(self, **properties):
@@ -11,3 +12,29 @@ class RowTemplate1(RowTemplate1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    # Assuming each row has a unique 'version' column
+    print(event_args)
+    #version = event_args['row']['version']
+    
+    # Get the file associated with this version ID
+    #file_row = app_tables.my_table_name.get(version=version)
+    
+    # Trigger the file download
+    #anvil.server.download(file_row['file'])
+
+  def button_3_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    #print(event_args) - doesn't get version data from row
+    version = self.item['version']
+    #print(version) - OK!
+    file_row = app_tables.files.get(version=version)
+    anvil.media.download(file_row['file'])
+
+
+
+
+
+  
